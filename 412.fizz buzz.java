9185de4c89b5dba21825e0ddf4ecaffe5c8291de
deepeshkumar06 <deepeshkumar.p2004@gmail.com>
2024-06-23 19:14:55 +0530

    fizz buzz added

---
+++ 412.fizz buzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> fizz = new ArrayList<>();
+        for(int i=1 ; i<=n ; i++){
+            if(i%3 == 0  && i%5==0){
+                fizz.add("FizzBuzz");
+            }
+            else if(i%5 == 0)
+                fizz.add("Buzz");
+            else if(i%3==0)
+                 fizz.add("Fizz");
+            else
+                fizz.add(Integer.toString(i));
+        }
+        return fizz;
+    }
+}
